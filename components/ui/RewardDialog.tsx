d98afd9946c8e44d19dcec51834bf6027cf9a180
Bikash0nath <128994295+Bikash0nath@users.noreply.github.com>
2026-02-16 16:30:54 +0530

    Added new section 'View All Product',Make all cards clickable

--- components/ui/RewardDialog.tsx
+++ components/ui/RewardDialog.tsx
@@ -6,7 +6,7 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/Button";
 import { TicketPercent } from "lucide-react";
 import { RewardItem } from "@/app/(dashboard)/redeem/models";
 
